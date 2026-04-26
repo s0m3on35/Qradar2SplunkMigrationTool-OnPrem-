c7d7ce3ea5bf48af3a8c697b98b57b771ed9fcdb
someone <218046406+s0m3on35@users.noreply.github.com>
2026-04-26 11:43:24 +0200

    Delete Qradar2Splunk_portable directory

--- Qradar2Splunk_portable/run_portable.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python convert.py   --input-csv samples/qradar_rules.csv   --xml samples/qradar_rules_export.xml   --outdir output   --app My_SIEM_Migration   --default-index main   --field-map mappings/field_map.yaml   --building-blocks mappings/building_blocks.csv   --reference-sets mappings/reference_sets.csv   --backup --log-level INFO
-echo "Listo. Revisa la carpeta 'output/'."
